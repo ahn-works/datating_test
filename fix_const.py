bc8174b5a7858b096daf0d021c56f7b2c3e8d3e1
DreamTech <dreamtech320@gmail.com>
2026-09-17 09:59:51 +0900

    fix: remove const Expanded to fix web compilation

---
+++ fix_const.py
@@ -0,0 +1,12 @@
+﻿import io
+import re
+
+with io.open('lib/screens/soso_car_host_screen.dart', 'r', encoding='utf-8') as f:
+    host = f.read()
+
+host = host.replace('const Expanded(\n                    child: Column(\n                      crossAxisAlignment: CrossAxisAlignment.start,\n                      children: [\n                        Row(\n                          children: [\n                            Text(', 'Expanded(\n                    child: Column(\n                      crossAxisAlignment: CrossAxisAlignment.start,\n                      children: [\n                        Row(\n                          children: [\n                            Text(')
+
+host = host.replace('const Expanded(\n                    child: Column(', 'Expanded(\n                    child: Column(')
+
+with io.open('lib/screens/soso_car_host_screen.dart', 'w', encoding='utf-8') as f:
+    f.write(host)
