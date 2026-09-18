efc013fd7785c643c3ee20b5c7d9b7cb0202b163
DreamTech <dreamtech320@gmail.com>
2026-09-18 15:51:48 +0900

    fix: correctly rewrite bottom navigation bar in home screen

--- lib/screens/soso_car_home_screen.dart
+++ lib/screens/soso_car_home_screen.dart
@@ -411,6 +411,20 @@ class _OOMUHomeScreenState extends State<OOMUHomeScreen> {
           label: '내 여행',
         ),
       ],
+          ),
+          label: '채팅',
+        ),
+        BottomNavigationBarItem(
+          icon: InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const OOMUMyTripsScreen()));
+            },
+            child: const Icon(Icons.directions_car_outlined)
+          ),
+          activeIcon: const Icon(Icons.directions_car),
+          label: '내 여행',
+        ),
+      ],
 ],
           ),
           label: '채팅',
